2556252e2f8de3d34f01ccec594001483be3af57
Sulfide <101655042+mSulfide@users.noreply.github.com>
2024-02-17 19:01:30 +0300

    class Graph

--- js/modules/Graph2D/Graph.js
+++ js/modules/Graph2D/Graph.js
@@ -1,62 +1,65 @@
-function Graph({ id, width = 300, height = 300, WIN, callbacks }) {
-    let canvas;
-    if (id) {
-        canvas = document.getElementById(id);
-    } else {
-        canvas = document.createElement('canvas');
-        document.querySelector('body').appendChild(canvas);
-    }
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext('2d');
-    const { wheel, mousemove, mouseleave, mouseup, mousedown } = callbacks | {
-        wheel: () => '',
-        mousemove: () => '',
-        mouseleave: () => '',
-        mouseup: () => '',
-        mousedown: () => ''
-    };
-    canvas.addEventListener('wheel', wheel);
-    canvas.addEventListener('mousemove', mousemove);
-    canvas.addEventListener('mouseleave', mouseleave);
-    canvas.addEventListener('mouseup', mouseup);
-    canvas.addEventListener('mousedown', mousedown);
-    const PI2 = 2 * Math.PI;
+class Graph {
+    constructor({ id, width = 300, height = 300, WIN, callbacks }) {
+        if (id) {
+            this.canvas = document.getElementById(id);
+        } else {
+            this.canvas = document.createElement('canvas');
+            document.querySelector('body').appendChild(canvas);
+        }
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.context = this.canvas.getContext('2d');
 
-    const xs = x => (x - WIN.left) / WIN.width * canvas.width;
+        this.WIN = WIN;
 
-    const ys = y => (-y + (WIN.bottom + WIN.height)) / WIN.height * canvas.height;
-
-    this.sx = x => x * WIN.width / canvas.width;
-
-    this.sy = y => -y * WIN.height / canvas.height
-
-    this.clear = () => {
-        context.fillStyle = '#efe';
-        context.fillRect(0, 0, canvas.width, canvas.height);
+        const { wheel, mousemove, mouseleave, mouseup, mousedown } = callbacks | {
+            wheel: () => '',
+            mousemove: () => '',
+            mouseleave: () => '',
+            mouseup: () => '',
+            mousedown: () => ''
+        };
+        this.canvas.addEventListener('wheel', wheel);
+        this.canvas.addEventListener('mousemove', mousemove);
+        this.canvas.addEventListener('mouseleave', mouseleave);
+        this.canvas.addEventListener('mouseup', mouseup);
+        this.canvas.addEventListener('mousedown', mousedown);
     }
 
-    this.line = (x1, y1, x2, y2, color = '#f00', width = 1) => {
-        context.beginPath();
-        context.strokeStyle = color;
-        context.lineWidth = width;
-        context.moveTo(xs(x1), ys(y1));
-        context.lineTo(xs(x2), ys(y2));
-        context.stroke();
-        context.closePath();
+    xs = x => (x - this.WIN.left) / this.WIN.width * canvas.width;
+
+    ys = y => (-y + (this.WIN.bottom + this.WIN.height)) / this.WIN.height * canvas.height;
+
+    sx = x => x * this.WIN.width / canvas.width;
+
+    sy = y => -y * this.WIN.height / canvas.height
+
+    clear = () => {
+        this.context.fillStyle = '#efe';
+        this.context.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-    this.point = (x, y, color = '#f00', size = 2) => {
-        context.beginPath();
-        context.strokeStyle = color;
-        context.arc(xs(x), ys(y), size, 0, PI2);
-        context.stroke();
-        context.closePath();
+    line = (x1, y1, x2, y2, color = '#f00', width = 1) => {
+        this.context.beginPath();
+        this.context.strokeStyle = color;
+        this.context.lineWidth = width;
+        this.context.moveTo(this.xs(x1), this.ys(y1));
+        this.context.lineTo(this.xs(x2), this.ys(y2));
+        this.context.stroke();
+        this.context.closePath();
     }
 
-    this.text = (x, y, text, color = "#000") => {
-        context.font = "24pt arial";
-        context.fillStyle = color;
-        context.fillText(text, xs(x), ys(y));
+    point = (x, y, color = '#f00', size = 2) => {
+        this.context.beginPath();
+        this.context.strokeStyle = color;
+        this.context.arc(this.xs(x), this.ys(y), size, 0, 2 * Math.PI);
+        this.context.stroke();
+        this.context.closePath();
+    }
+
+    text = (x, y, text, color = "#000") => {
+        this.context.font = "24pt arial";
+        this.context.fillStyle = color;
+        this.context.fillText(text, this.xs(x), this.ys(y));
     }
 }
